2360b5b6717f3ca3a5f36448e7ed301889d362f9
Allan Kiplagat <allankiplagatkipkemei@gmail.com>
2023-09-29 14:37:19 +0300

    fix cors errors

--- ShareIt-main/backend/database/database.js
+++ ShareIt-main/backend/database/database.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env
+dotenv.config();
 
 class Database {
-    constructor() {
-        this._connect()
-    }
+  constructor() {
+    this._connect();
+  }
 
-    _connect() {
-        const db_string = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-        mongoose.connect(db_string)
-            .then(() => {
-                console.log('[*] Connection to DB successful')
-            })
-            .catch(err => {
-                console.error('[!] Error connecting to DB: ', err)
-            })
-    }
+  _connect() {
+    const db_string = process.env.DB_STRING;
+    mongoose
+      .connect(db_string)
+      .then(() => {
+        console.log("[*] Connection to DB successful");
+      })
+      .catch((err) => {
+        console.error("[!] Error connecting to DB: ", err);
+      });
+  }
 }
 
-module.exports = new Database()
+module.exports = new Database();
